fa748b765e16819090b58cade1460858c04300f1
Peter Cheng <peter.cheng@e2open.com>
2022-11-23 15:11:07 -0500

    Complete routing on backend

--- back-end/index.js
+++ back-end/index.js
@@ -2,23 +2,43 @@ const express = require('express');
 const cors = require('cors')
 const server = express();
 const http = require('http');
+const bodyParser = require('body-parser')
+
 
 server.use(cors());
+server.use(bodyParser());
 
 const DB = {
-    comments: [
-        {name: "Jonny", comment: "A great product!"},
-        {name: "Jessica", comment: "Not really!"},
-    ]
+    "111111": [  ],
+    "222222": []
 };
 
+/**
+ * Backend-fronend communication using REST API
+ * GET, POST, DELETE, UPDATE
+ */
 server.get('/', function (req, res) {
   res.send('Hello World');
 });
 
-server.get('/comments', function (req, res) {
+
+server.get('/comments/:id', function (req, res) {
+    const id = req.params.id;
+
+    const comments = DB[id] ? DB[id] : [];
+    
     res.status = 200;
-    res.json(DB.comments);
+    res.json(comments);
+});
+
+server.post('/comments', function (req, res) {
+    const { id, name, comment } = req.body
+    if(DB[id] === undefined) {
+        DB[id] = [];
+    }
+    DB[id].push({ name, comment });
+    res.status(200);
+    res.json({ name, comment });
 });
 
 
--- front-end/index.js
+++ front-end/index.js
@@ -1,8 +1,55 @@
 const searchParam = window.location.search;
-const [id, value] = searchParam.replace("?", "").split("=");
-console.log(id, value);
+const [_, id] = searchParam.replace("?", "").split("=");
 
-fetch("http://localhost:3000/comments")
+
+fetch(`http://localhost:3000/comments/${id}`)
 .then(responce => responce.json())
-.then(data => console.log(data))
+.then(comments => renderComments(comments))
 .catch(err => console.log(err))
+
+
+/**
+ * Google
+ * DOM API for Javascript
+ * DOM events using JavaScript
+ */
+const renderComments = function (comments) {
+    const container = document.getElementById("comments-section");
+
+    comments.forEach(function(data) {
+        const commentCard = document.createElement("div");
+        commentCard.setAttribute("class", "card");
+    
+        const commentBody = document.createElement("div");
+        commentBody.setAttribute("class", "card-body");
+        
+        commentBody.appendChild(document.createTextNode(`${data.name}: ${data.comment}`));
+    
+        commentCard.appendChild(commentBody);
+
+        container.appendChild(commentCard);
+    });
+};
+
+const nameInput = document.getElementById("name");
+const commetnInput = document.getElementById("comment");
+const saveButton = document.getElementById("save-button");
+
+/**
+ * Google
+ * Headers for fetch requests
+ */
+saveButton.addEventListener("click", function() {
+    const name = nameInput.value;
+    const comment = commetnInput.value;
+    fetch("http://localhost:3000/comments", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({id, name, comment})
+    })
+    .then(responce => responce.json())
+    .then(comment => renderComments([comment]))
+    .catch(err => console.log(err))
+});
